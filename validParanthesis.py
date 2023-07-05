bc7fab599d78a933858a4e9034aa98ae78dcf9d3
Ruthika <ruthirockstar2218@gmail.com>
2023-07-05 22:59:51 +0530

    problem 20

---
+++ validParanthesis.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        k='([{'
+        stk=[]
+        for i in s:
+            if( i in k ):
+                stk.append(i)
+            else:
+                if( not stk):
+                    return False
+                if(i == ')' and stk[-1] == '('):
+                    stk.pop()
+                elif (i=='}' and stk[-1] == '{'):
+                    stk.pop()
+                elif (i== ']' and stk[-1] == '['):
+                    stk.pop()
+                else:
+                    return False
+        return (not stk)
